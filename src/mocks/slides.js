da30332bc6d62e3abe7f9a3ea3b59bf83914af1d
felipe andre <felipeandre8@outlook.com>
2022-10-08 17:58:48 -0300

    carousel categories create, mock add and index update

---
+++ src/mocks/slides.js
@@ -0,0 +1,15 @@
+export const slides = [
+  {
+    url: "/images/logo.png",
+    title: "Slide-1",
+  },
+  {
+    url: "localhost:3000/",
+    title: "Slide-2",
+  },
+  {
+    url: "./assets/images/slide3.png",
+    title: "Slide-3",
+  },
+];
+
