eb4ad818f3274f1cf3d51424e1ac0b82d4e529a5
felipe andre <felipeandre8@outlook.com>
2022-10-09 18:49:17 -0300

    carousel update, imgs update size, product categories update alt

--- src/components/CategoriesCarousel/index.jsx
+++ src/components/CategoriesCarousel/index.jsx
@@ -1,8 +1,49 @@
-import React from "react";
-import { CategoriesTitles } from './styles'
+import React from 'react';
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import data from '../../mocks/productCategories'
+import { CategoriesTitles, Container, CategoriesCard, Card, CardTitle, CardImg } from './styles'
+import "swiper/css";
+import "swiper/css/pagination";
 
 export const CategoriesCarousel = () => {
+  const { dataCategories } = data; 
   return (
-    <CategoriesTitles> Categories </CategoriesTitles>
+    <Container> 
+      <CategoriesTitles> Categories </CategoriesTitles>
+       
+        {dataCategories.length > 0 ? (
+          <CategoriesCard>
+            <Swiper
+              slidesPerView={4}
+              spaceBetween={12}
+              className="mySwiper">
+              
+              {dataCategories.map((item) => {
+                if (item.type === 'categories-icon'){
+                  return (
+                    <SwiperSlide key={item.id}>
+
+                      <Card>
+                        <CardImg>
+                          <img 
+                            src={item.image} alt={item.alt }/> 
+                          <CardTitle>
+                            {item.name}
+                          </CardTitle> 
+                        </CardImg>
+                      </Card>
+
+                    </SwiperSlide>
+                  );
+                }
+                return null; 
+              })}
+
+            </Swiper>
+          </CategoriesCard>
+        ) : null }  
+        
+    </Container>
   )
 }
