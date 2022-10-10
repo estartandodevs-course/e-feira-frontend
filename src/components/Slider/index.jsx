1e44f85190c17673dad820cfb6b7d23f1f07ce62
junglereef <atila.iglesias@hotmail.com>
2022-10-10 11:55:28 -0300

    home carrossel destaque

--- src/components/Slider/index.jsx
+++ src/components/Slider/index.jsx
@@ -1,31 +1,40 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css'; 
+import { SliderBox } from './styles';
+import 'swiper/css';
 import 'swiper/css/pagination';
-import { Pagination } from "swiper";
+import { Pagination } from 'swiper';
 
 const SlidesComponent = ({ slides }) => {
   return (
     <>
-        <Swiper 
-        pagination={{
-          dynamicBullets:true,
-        }} 
-        modules={[Pagination]}  
-        className="swiperSection"
-        > 
-          
-          <SwiperSlide><img src={slides[0].url} alt="" /></SwiperSlide>
-          <SwiperSlide><img src={slides[1].url} alt="" /></SwiperSlide>
-          <SwiperSlide><img src={slides[2].url} alt="" /></SwiperSlide>
-        
+      <SliderBox>
+        <Swiper
+          spaceBetween={30}
+          loop={true}
+          pagination={{
+            el: '.swiper-custom-pagination',
+            // dynamicBullets: true,
+            clickable: true,
+          }}
+          modules={[Pagination]}
+          className="swiperSection"
+        >
+          <SwiperSlide>
+            <img src={slides[0].url} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={slides[1].url} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={slides[2].url} alt="" />
+          </SwiperSlide>
+        <div className="swiper-custom-pagination"/>
         </Swiper>
-
+      </SliderBox>
     </>
   );
 };
 
-
-
-export { SlidesComponent }; 
+export { SlidesComponent };
---
+++ src/components/Slider/styles.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+
+
+export const SliderBox = styled.div`
+.swiper-custom-pagination{
+  margin-top: 2vh;
+  text-align: center;
+}
+
+.swiper-custom-pagination>.swiper-pagination-bullet {
+  margin-left: 25px;
+  color: #32A060;
+  background-color: #32A060;
+}
+`
