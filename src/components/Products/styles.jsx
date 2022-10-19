a0b948e4f1631092c0ec558913748f4a46252dc2
felipe andre <felipeandre8@outlook.com>
2022-10-19 19:26:23 -0300

    fixed supplier page and update products page

--- src/components/Products/styles.jsx
+++ src/components/Products/styles.jsx
@@ -17,4 +17,70 @@ export const Card = styled.div`
   justify-content: center;
   width: 100%;
   background-color: white;
+  flex-direction: column;
+  > img {
+    padding: 3.75rem 1rem 1.31rem 1rem;
+  }
 `;
+
+export const CardTitle = styled.h6`
+  text-align: start;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 120%;
+  margin: 0 1rem;
+  padding: 0;
+`;
+
+export const CardSubTitle = styled.h6`
+  text-align: start;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 120%;
+  margin: 0 1rem;
+`;
+
+export const TextInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 1rem 0;
+`;
+
+export const TextQuestion = styled.h6`
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 120%;
+  padding: 0 1rem;
+`;
+
+export const CardPrice = styled.div`
+  background: rgba(59, 160, 50, 0.2);
+  width: 20%;
+  height: auto;
+  border-radius: 6px;
+  padding: 0.5rem 1rem;
+  margin: 1rem 0;
+  display: flex;
+
+  text-align: center;
+  align-items: center;
+
+  & > p {
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 120%;
+    text-align: center;
+    letter-spacing: 0.0125em;
+    text-transform: uppercase;
+    color: #fffff;
+  }
+`;
+
+export const CardInformations = styled.div``;
+
+export const MeetSupplier = styled.div``;
+
+export const PriceCompare = styled.div``;
