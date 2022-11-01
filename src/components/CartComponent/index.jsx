c0cc5979f43399a9594518126c3cd4eab4390461
felipe andre <felipeandre8@outlook.com>
2022-11-01 18:58:31 -0300

    fixes pages

--- src/components/CartComponent/index.jsx
+++ src/components/CartComponent/index.jsx
@@ -1,7 +1,5 @@
 import { useCart } from '../../contexts/CartContext';
 
-import { Container, ProductsContainer, ProductItem, Card, CardSubTitle, CardTitle, CardRight } from './styles';
-=======
 import {
   Container,
   ProductsContainer,
@@ -9,12 +7,9 @@ import {
   ProductImage,
   ProductName,
   ProductQty,
-  ProductPrice,
+  // ProductPrice,
   ProductTotal,
 } from './styles';
-
-
-import { Link } from 'react-router-dom';
 
 export const CartComponent = () => {
   const { cart } = useCart();
@@ -26,31 +21,16 @@ export const CartComponent = () => {
       <ProductsContainer>
         {cart.map(item => (
           <ProductItem key={item.id}>
-
-            <Card>
-              <img src={item.image} />
-            </Card>
-            <Link to={`/produtos/${item.id}`}>{item.name}</Link>
-            <div>
-              <CardTitle>{item.title}</CardTitle>
-              <div>
-                <CardSubTitle>{item.subtitle}</CardSubTitle>
-              </div>
-            </div>
-
-            <div>{item.price}</div>
-            <CardRight></CardRight>
-
             <a href={`produtos/${item.id}`}>
               <ProductImage>
                 <img src={item.image} />
               </ProductImage>
+
               <ProductName>{item.name}</ProductName>
-              <ProductPrice>Preço por unidade: {item.price} </ProductPrice>
+              {/* <ProductPrice>Preço por unidade: {item.price} </ProductPrice> */}
               <ProductQty>Quantidade: {item.amount}</ProductQty>
               <ProductTotal>Total: {item.amount * item.price}R$ </ProductTotal>
             </a>
-
           </ProductItem>
         ))}
       </ProductsContainer>
