8fca1855c511eb623e23e8c049f463c966180a43
Atila Iglesias <atila.iglesias@hotmail.com>
2022-11-04 17:33:12 -0300

    search logic updated

--- src/components/SearchComp/index.jsx
+++ src/components/SearchComp/index.jsx
@@ -1,15 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import {
-  CardDescription,
-  CardImg,
-  CardLeft,
-  CardRight,
-  CardSubTitle,
-  CardTitle,
-  Container,
-  ItemPrice,
-  SearchBox,
-} from './styles';
+import { Container, SearchBox } from './styles';
 import productsApi from '../../services/products';
 import { ProductsContainer, ProductItem } from './styles';
 import { InputAdornment, OutlinedInput } from '@mui/material';
@@ -18,15 +8,7 @@ import SearchIcon from '@mui/icons-material/Search';
 export const SearchComp = () => {
   const { listProducts } = productsApi;
   const [products, setProducts] = useState([]);
-  const [inputBoxData, setinputBoxData] = useState('');
-  const [filterValue, setFilterValue] = useState(false);
-  // const category_id = 1;
-
-  // const test = 'ç';
-  // const test1 = 'c';
-
-  // const testResult = test.normalize('NFKD') == test1.normalize('NFKD');
-  // console.log(testResult);
+  const [showBoxData, setshowBoxData] = useState('');
 
   useEffect(() => {
     const loadProducts = async () => {
@@ -38,60 +20,42 @@ export const SearchComp = () => {
     loadProducts();
   }, []);
 
-  useEffect(() => {
-    (() => {
-      if (inputBoxData == '') {
-        console.log('Empty Array');
-      } else {
-        setFilterValue;
-      }
-    })();
-  }, [inputBoxData]);
-
   return (
     <Container>
       <SearchBox>
         <OutlinedInput
           sx={{ fontSize: 20 }}
           type="text"
-          value={inputBoxData}
-          letterSpacing="0.001em"
+          fontSize=""
+          value={showBoxData}
           placeholder="Insira o que procura"
           startAdornment={
             <InputAdornment position="start">
-              <SearchIcon style={{ fontSize: '30' }} />
+              <SearchIcon />
             </InputAdornment>
           }
           onChange={({ currentTarget }) =>
-            setinputBoxData(String(currentTarget.value.normalize('NFKD').replace(/[^\w]/g, '')))
+            setshowBoxData(String(currentTarget.value.normalize('NFKD').replace(/[^\w]/g, '')))
           }
         />
       </SearchBox>
 
-      <ProductsContainer>
-        {products
-          .filter(item =>
-            item.name.normalize('NFKD').replace(/[^\w]/g, '').toLowerCase().includes(filterValue.toLowerCase())
-          )
-          .map(item => (
-            <ProductItem key={item.id}>
-              <a href={`produtos/${item.id}`}>
-                <CardLeft>
-                  <CardImg>
-                    <img src={item.image} />
-                  </CardImg>
-                </CardLeft>
-                <CardRight>
-                  <CardDescription>
-                    <CardTitle>{item.name}</CardTitle>
-                    <CardSubTitle>{item.subtitle}</CardSubTitle>
-                    <ItemPrice>R$ {item.price.toFixed(2)}</ItemPrice>
-                  </CardDescription>
-                </CardRight>
-              </a>
-            </ProductItem>
-          ))}
-      </ProductsContainer>
+      {showBoxData !== '' && (
+        <ProductsContainer>
+          {products
+            .filter(item =>
+              item.name.normalize('NFKD').replace(/[^\w]/g, '').toLowerCase().includes(showBoxData.toLowerCase())
+            )
+            .map(item => (
+              <ProductItem key={item.id}>
+                <a href={`produtos/${item.id}`}>
+                  <img src={item.image} />
+                  {item.name}
+                </a>
+              </ProductItem>
+            ))}
+        </ProductsContainer>
+      )}
     </Container>
   );
 };
