fa96c06ea181730b95dafce38c64082df89da718
Atila Iglesias <atila.iglesias@hotmail.com>
2022-11-09 20:04:05 -0300

    created orderPage

--- src/components/CartComponent/index.jsx
+++ src/components/CartComponent/index.jsx
@@ -57,24 +57,34 @@ export const CartComponent = () => {
   }, [cart]);
 
   const postProduct = () => {
-    const data = {
-      order: {
-        address: cartGrouped[0].value[0].address,
-        payment_method: 'Dinheiro',
-        total_price: cartGrouped[0].value[0].totalAmount,
-        grandTotal: cartGrouped[0].value[0].grandTotal,
-        delivery_tax: cartGrouped[0].value[0].freight,
-      },
-      order_itens: [
-        {
-          amount: cartGrouped[0].value[0].amount,
-          individual_price: cartGrouped[0].value[0].price,
-          product_id: cartGrouped[0].value[0].id,
+    const data = [
+      {
+        order: {
+          address: cartGrouped[0].value[0].address,
+          payment_method: 'Dinheiro',
+          total_price: cartGrouped[0].value[0].totalAmount,
+          grand_total: cartGrouped[0].value[0].grandTotal,
+          delivery_tax: cartGrouped[0].value[0].freight,
         },
-      ],
-    };
+        order_itens: [
+          {
+            amount: cartGrouped[0].value[0].amount,
+            individual_price: cartGrouped[0].value[0].price,
+            product_id: cartGrouped[0].value[0].id,
+          },
+        ],
+      },
+    ];
 
-    ApiServer.post(`checkout`, data)
+    ApiServer.post(
+      'checkout',
+      { data },
+      {
+        headers: {
+          userid: 1,
+        },
+      }
+    )
       .then(response => {
         console.log(response);
       })
