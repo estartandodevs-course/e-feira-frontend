8d66dbfc7b857426cdf6504ea1e028f014449557
felipe andre <felipeandre8@outlook.com>
2022-11-10 19:46:26 -0300

    purchase notification created

--- src/components/PurchaseAlert/index.jsx
+++ src/components/PurchaseAlert/index.jsx
@@ -1,25 +1,47 @@
-import React from 'react';
-import Swal from 'sweetalert2';
-import { AlertTitles, ButtonMessage, Container, Img, Message } from './styles';
+import { Button, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions } from '@mui/material';
+import { AlertTitles, ButtonMessage, Img, Message } from './styles';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import EfeiraIcon from '../../assets/images/efeira-icon';
 
 export const PurchaseAlert = () => {
-  const Swal = require('sweetalert2');
-
-  Swal.fire({
-    title: 'Error!',
-    text: 'Do you want to continue',
-    icon: 'error',
-    confirmButtonText: 'Cool',
-  });
+  const [open, setOpen] = useState(false);
 
   return (
     <>
-      <Container>
-        <Img></Img>
-        <AlertTitles></AlertTitles>
-        <Message></Message>
-        <ButtonMessage></ButtonMessage>
-      </Container>
+      {/* <Button onClick={() => setOpen(true)}>Open Dialog</Button> */}
+      <Dialog
+        aria-labelldby="dialog-title"
+        aria-describedby="dialog-description"
+        open={open}
+        onClose={() => setOpen(false)}
+      >
+        <DialogTitle id="dialog-title">
+          <Img>
+            <EfeiraIcon />
+          </Img>
+
+          <AlertTitles>Pedido Concluído com sucesso !</AlertTitles>
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="dialog-description">
+            <Message>
+              Seu pedido foi efetuado com sucesso. Fique de olho no seu e-mail que você receberá uma mensagem com os
+              detalhes da sua compra. Seu pedido chegará em sua casa em até 7 dias úteis.
+            </Message>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions style={{ justifyContent: 'center' }}>
+          {/* <Button onClick={() => setOpen(false)}> Cancel</Button> */}
+          <ButtonMessage>
+            <Button autoFocus className="btn-back">
+              <Link onClick={() => setOpen(false)} to={`/`}>
+                Voltar para a tela inicial
+              </Link>
+            </Button>
+          </ButtonMessage>
+        </DialogActions>
+      </Dialog>
     </>
   );
 };
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,21 +5,12 @@ import { Highlights } from '../../components/Highlights';
 import { CategoriesCarousel } from '../../components/Carousel';
 import { Latest } from '../../components/Latest';
 import Navbar from '../../components/Navbar';
-import withReactContent from 'sweetalert2-react-content';
-import Swal from 'sweetalert2';
+import { PurchaseAlert } from '../../components/PurchaseAlert';
 
 const Home = () => {
   const sleep = (ms = 1000) => new Promise(resolve => setTimeout(resolve, ms));
 
   const { hideSplash, isShown } = useSplash();
-
-  const MySwal = withReactContent(Swal);
-
-  MySwal.fire({
-    title: <strong>Good job!</strong>,
-    html: <i>You clicked the button!</i>,
-    icon: 'success',
-  });
 
   // SlashWindow
   useEffect(() => {
@@ -32,6 +23,7 @@ const Home = () => {
   return isShown ? null : (
     <>
       <ContainerStyle>
+        <PurchaseAlert />
         <Highlights />
         <CategoriesCarousel />
         <Latest />
